d1d04efee9cd2428ae0b780272b42aa5ea0086e9
vinobaje <mvinobaje@gmail.com>
2015-02-27 13:15:19 -0500

    Planet class created

---
+++ Vinobaje_Manohar_Assingment_3/Planet.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Assingment03
+{
+    abstract class Planet
+    {
+        double _diameter;
+        double _mass;
+        int _moonCount;
+        string _name;
+        double _orbitalPeriod;
+        int _ringCount;
+        double _rotationPeriod;
+
+        public double Diameter
+        {
+            get //read-only
+            {
+                return this._diameter;
+            }
+
+        }
+
+        public double Mass
+        {
+            get //read-only
+            {
+                return this._mass;
+            }
+
+        }
+
+        public int moonCount
+        {
+            get //read-only
+            {
+                return this._moonCount;
+            }
+
+        }
+
+        public string Name
+        {
+            get //read-only
+            {
+                return this._name;
+            }
+
+        }
+
+        public double orbitalPeriod
+        {
+            get //read-only
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        public int ringCount
+        {
+            get //read-only
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public double rotationPeriod
+        {
+            get //read-only
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " is a planet, " + this.Diameter + " is it's diameter and " + this.Mass + " is it's mass.";
+        }
+    }
+}
