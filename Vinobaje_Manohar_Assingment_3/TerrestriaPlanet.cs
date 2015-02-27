76fdf7aebba481acddc9d853cdbebd44eec281be
vinobaje <mvinobaje@gmail.com>
2015-02-27 13:18:54 -0500

    TerrestriaPlanet created

---
+++ Vinobaje_Manohar_Assingment_3/TerrestriaPlanet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Assingment03Vinobaje_Manohar_Assingment_3
+{
+    class TerrestriaPlanet : Planet, IHasMoons, IHabitable
+    {
+        private bool _oxygen;
+
+
+        public TerrestriaPlanet(bool oxygen, string name, double diameter, double mass)
+            : base(name, diameter, mass)
+        {
+            this._oxygen = oxygen; //local variable
+        }
+
+        public bool HasMoons()
+        {
+            return (moonCount > 0) ? true : false;
+        }
+
+        public bool Habitable()
+        {
+            return (_oxygen == true) ? true : false;
+        }
+    }
+}
