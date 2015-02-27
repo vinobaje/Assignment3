a71bcd122ced6a31dc75b42478e788c0f6b6a0c6
vinobaje <mvinobaje@gmail.com>
2015-02-27 13:24:21 -0500

    Finished Main method

--- Vinobaje_Manohar_Assingment_3/Program.cs
+++ Vinobaje_Manohar_Assingment_3/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Vinobaje_Manohar_Assingment_3
+
+namespace Assingment03Vinobaje_Manohar_Assingment_3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GiantPlanet Jupiter = new GiantPlanet("Gas", "Jupiter", 5000000, 80000000);
+            TerrestriaPlanet earth = new TerrestriaPlanet(true, "Earth", 9.6786870000, 178908989);
+
+
+            Console.Write("\n");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("******************************* Giant Planet ******************************");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+
+            Console.WriteLine(Jupiter.ToString());
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("*************************** Terrestrial Planet ****************************");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+            Console.WriteLine(earth.ToString());
+            WaitForAnyKey();
+        }
+
+        public static void WaitForAnyKey()
+        {
+            Console.WriteLine("Please press any key to exit....");
+            Console.ReadKey();
         }
     }
 }
